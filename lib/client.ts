ca76353208a3076c43c916d1c22d697fddaed24f
CodePwn <77036672+CodePwn2021@users.noreply.github.com>
2022-08-26 16:41:19 +0800

    add doc

--- lib/client.ts
+++ lib/client.ts
@@ -6,7 +6,7 @@ import log4js from 'log4js';
 const pkg = require('../package.json');
 const DEFAULT_WS_URL: string = 'ws://localhost:24011/omega_side';
 
-/** 一个客户端 **/
+/** 一个客户端 */
 export class OmegaClient extends EventEmitter {
     bot_name: String = 'Unknown'
     private connected: Boolean = false
@@ -16,31 +16,31 @@ export class OmegaClient extends EventEmitter {
     constructor(host: string, level:LogLevel = 'mark') {
         super();
         this.wsClient = new WebSocket(host);
-        /** 注册&处理ws client的事件 **/
+        /** 注册&处理ws client的事件 */
         this.wsClient.on('open', async () => {
             this.connected = true;
             // 处理omega的包
             this.wsClient.on('message', (message) => {
                 const obj_message = JSON.parse(message.toString('utf-8'));
-                /** omega 响应数据包 **/
+                /** omega 响应数据包 */
                 if (obj_message.client !== 0) {
-                    /** 发送的数据包有问题 **/
+                    /** 发送的数据包有问题 */
                     if(obj_message.violate) {
                         throw Error(JSON.stringify(obj_message))
                     }
                     this.emit('omega.response', obj_message);
                     return;
                 }
-                /** omega 主动推送数据包 **/
+                /** omega 主动推送数据包 */
                 this.packetCount = obj_message.client;
                 this.emit('omega.push', obj_message);
             });
 
-            /** 获取自己（机器人）的昵称 **/
+            /** 获取自己（机器人）的昵称 */
             const get_name_packet = await this.execCmd('tell @s @s');
             this.bot_name = get_name_packet.data.result.OutputMessages[0].Parameters[0];
 
-            /** 初始化logger **/
+            /** 初始化logger */
             this.logger = log4js.getLogger(`[LightOmega:${this.bot_name}]`);
             this.logger.level = level;
             this.logger.mark('┌────────────────────────────────────────────────────────────────┐');
@@ -48,10 +48,10 @@ export class OmegaClient extends EventEmitter {
             this.logger.mark('| changelog: https://github.com/CodePwn2021/light-omega/releases |');
             this.logger.mark('└────────────────────────────────────────────────────────────────┘');
 
-            /** 准备就绪 **/
+            /** 准备就绪 */
             this.emit('omega.ready');
         });
-        // 错误提示
+        /** 错误提示 */
         this.wsClient.on('error', (err) => {
             console.error('ERROR !!!!!!!!!!!!!!');
             console.error(err);
@@ -61,8 +61,13 @@ export class OmegaClient extends EventEmitter {
 
     private packetCount: number = 0
 
-    /** 发送一个数据包 **/
-    async sendPacket(func: string, param: object): Promise<OmegaResponsePacket> {
+    /**
+     * 发送一个数据包
+     * @function sendPacket
+     * @param {string} func 请求的数据类型（也可理解成函数名）
+     * @param {any} param 参数
+     */
+    async sendPacket(func: string, param: any): Promise<OmegaResponsePacket> {
         if(!this.connected) throw Error('WebSocket not connected!!!\nPlease use sendPacket() in client.on("omega.ready") !!!');
         if(this.packetCount === 24011 || this.packetCount === 0) {
             this.packetCount = 1;
@@ -88,7 +93,12 @@ export class OmegaClient extends EventEmitter {
         });
     }
 
-    /** 执行一个命令 **/
+    /**
+     * 执行一个命令
+     * @function execCmd
+     * @param {string} cmd 命令
+     * @param {boolean} isWebSocket (可选) 是否以游戏内 WebSocket 的身份执行
+     */
     async execCmd(cmd:string, isWebSocket:boolean = false):Promise<OmegaResponsePacket> {
         return await this.sendPacket(isWebSocket?'send_ws_cmd':'send_player_cmd', {
             cmd
@@ -96,7 +106,12 @@ export class OmegaClient extends EventEmitter {
     }
 }
 
-/** 创建一个客户端 **/
+/**
+ * 创建一个客户端
+ * @function createClient
+ * @param {string} host (可选) 自定义连接的ip/域名和端口
+ * @param {LogLevel} level (可选) 日志等级，参考log4js
+ */
 export function createClient(host?: string, level?: LogLevel) {
     let _host;
     if (host) {
